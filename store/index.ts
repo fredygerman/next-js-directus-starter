415f1e267ae73b9f36978c596d4e5cf61ad58a76
Fredy <fredgerman30@gmail.com>
2023-09-13 23:44:09 +0300

    wip : adding State Management

---
+++ store/index.ts
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducers';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from './storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: process.env.NODE_ENV !== 'production'
+});
+
+export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
---
+++ store/reducers.ts
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux';
+import type { AnyAction, Reducer } from 'redux';
+import type { RootState } from '.';
+
+import auth from './slices/auth';
+
+
+const reducers = combineReducers({
+      auth,
+});
+
+const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
+      if (action.type === 'auth/logoutUser' || action.type === 'admin/logout') {// check for action types
+            state = {} as RootState;
+      }
+      return reducers(state, action);
+};
+
+export default rootReducer;
