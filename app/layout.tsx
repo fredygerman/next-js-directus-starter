38dff3deb400ecbfba3370840cedcfdc6700aaa2
Fredy <fredgerman30@gmail.com>
2023-09-15 01:45:43 +0300

    updated Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,18 +11,53 @@ import { TailwindIndicator } from "@/components/misc/tailwind-indicator"
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+    "TypeScript",
+    "Directus",
+    "Headless CMS",
+  ],
+  authors: [
+    {
+      name: `${siteConfig.author ? siteConfig.author : "@fredygerman_"}`,
+      url: `${
+        siteConfig.authorUrl ? siteConfig.authorUrl : "https://fredygerman.com"
+      }`,
+    },
+  ],
+  creator: `${siteConfig.author ? siteConfig.author : "@fredygerman_"}`,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.links.home,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.links.home}/og.jpg`],
+    creator: `${siteConfig.author ? siteConfig.author : "@fredygerman_"}`,
+  },
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
+  manifest: `${siteConfig.links.home}/site.webmanifest`,
 }
 
 interface RootLayoutProps {
