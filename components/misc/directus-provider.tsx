2f7ae3f2f584f7a7f7e669c5b91dbe7dff4cfb71
Fredy <fredgerman30@gmail.com>
2023-09-21 08:46:58 +0300

    added React Directus Provider

--- components/misc/directus-provider.tsx
+++ components/misc/directus-provider.tsx
@@ -3,11 +3,17 @@
 import * as React from "react"
 import { DirectusProvider } from "react-directus"
 
+import { env } from "@/env.mjs"
+
+// import { createDirectus, authentication, rest, login } from '@directus/sdk';
+
 // import { MyCollections } from "@/types/directus-types"
 
-export function MyDirectusProvider({ children, ...props }: any) {
+export function MyDirectusProvider({ children }: any) {
+  // const client = createDirectus('directus_project_url').with(authentication()).with(rest());
+
   return (
-    <DirectusProvider apiUrl="https://api.example.com" options={{}}>
+    <DirectusProvider apiUrl={`${env.DIRECTUS_URL}`} options={{}}>
       {children}
     </DirectusProvider>
   )
