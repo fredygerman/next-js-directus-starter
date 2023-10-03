24aae1e5db1de2435ebf7adce6a4bbbc5609b7a4
Fredy <fredgerman30@gmail.com>
2023-10-03 20:27:46 +0300

    fix : posthog provider not tracking

--- components/misc/posthog-provider.tsx
+++ components/misc/posthog-provider.tsx
@@ -6,8 +6,8 @@ import { PostHogProvider } from "posthog-js/react"
 export function CustomPostHogProvider({ children }: any) {
   // Check that PostHog is client-side (used to handle Next.js SSR)
   if (typeof window !== "undefined") {
-    posthog.init(process.env.POSTHOG_KEY as string, {
-      api_host: process.env.POSTHOG_HOST_URL,
+    posthog.init(process.env.POSTHOG_KEY || "", {
+      api_host: process.env.POSTHOG_HOST_URL || "https://app.posthog.com",
       // Enable debug mode in development
       loaded: (posthog) => {
         if (process.env.NODE_ENV === "development") posthog.debug()
